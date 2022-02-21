91b65eb21795f05e9fa7216ac91442aff4ccc045
Joao Lucas Goncalves <joaolucasgc@icloud.com>
2022-02-21 13:43:07 -0300

    Category

--- src/main/java/com/test/proj1/config/TestConfig.java
+++ src/main/java/com/test/proj1/config/TestConfig.java
@@ -3,9 +3,11 @@ package com.test.proj1.config;
 import java.time.Instant;
 import java.util.Arrays;
 
+import com.test.proj1.entities.Category;
 import com.test.proj1.entities.Order;
 import com.test.proj1.entities.User;
 import com.test.proj1.entities.enums.OrderStatus;
+import com.test.proj1.repositories.CategoryRepository;
 import com.test.proj1.repositories.OrderRepository;
 import com.test.proj1.repositories.UserRepository;
 
@@ -24,8 +26,17 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
