9a9da69a373db2d42d33107549f6b04fa6020119
Joao Lucas Goncalves <joaolucasgc@icloud.com>
2022-02-23 10:57:45 -0300

    Product-OrderItem OneToMany association

--- src/main/java/com/test/proj1/entities/pk/OrderItemPK.java
+++ src/main/java/com/test/proj1/entities/pk/OrderItemPK.java
@@ -6,7 +6,6 @@ import javax.persistence.Embeddable;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.test.proj1.entities.Order;
 import com.test.proj1.entities.Product;
 
